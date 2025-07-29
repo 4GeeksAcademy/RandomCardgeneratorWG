0da78f2fc211f15a4d1448cf09b59af4f37231dc
WillGuell <william.guell001@gmail.com>
2025-07-29 03:41:39 +0000

    RandomCardGeneratorFinal

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  generateCard();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#newCardButton").addEventListener("click", generateCard); 
 };
+
+function generateCard(){
+
+  const suit = generateRandomSuit();
+  const number = generateRandomNumber();
+
+  
+  document.querySelector(".top-suit").textContent = suit;
+  document.querySelector(".bottom-suit").textContent = suit;
+  document.querySelector(".number").textContent = number;
+
+  
+  const card = document.querySelector(".card");
+  if (suit === "♥" || suit === "♦") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
+}
+
+let generateRandomNumber = () => {
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let index = Math.floor(Math.random() * numbers.length);
+  return numbers[index];
+};
+
+let generateRandomSuit = () => {
+  let suits = ['♠', '♣', '♥', '♦'];
+  let index = Math.floor(Math.random() * suits.length);
+  return suits[index];
+};
+
+
+
